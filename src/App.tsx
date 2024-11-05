a883d8b30f5bd3e6e2ddd6f41e18902eafa97aa9
Hexi1997 <18715156450@163.com>
2024-11-05 18:09:35 +0800

    feat: support sign message

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,19 @@ import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import { OKXUniversalConnectUI } from "@okxconnect/ui";
 
-
-const chainId = '1'
+// b2 mainnet chainId
+const chainId = "223";
+const addressStorageKey = "login_address";
 function App() {
-  const [address, setAddress] = useState("");
+  const [address, setAddress] = useState<string>(
+    () => localStorage.getItem(addressStorageKey) || ""
+  );
   const [client, setClient] = useState<OKXUniversalConnectUI | null>(null);
   const initRef = useRef(false);
 
   useEffect(() => {
     async function initClient() {
-      if(initRef.current) return;
+      if (initRef.current) return;
       initRef.current = true;
       try {
         const client = await OKXUniversalConnectUI.init({
@@ -22,29 +25,31 @@ function App() {
           },
           actionsConfiguration: {
             returnStrategy: "tg://resolve",
-            modals: "all",
             tmaReturnUrl: "back",
-          }
+          },
         });
         setClient(client);
+        // 虽然每次页面加载 client 都会重新初始化，但是只要不要执行 client.disconnect, session 就不会变
+        console.log("session", client.session);
       } catch (e) {
         console.error(e);
       } finally {
-        initRef.current = false
+        initRef.current = false;
       }
     }
     initClient();
-  },[]);
+  }, []);
 
   return (
     <div className="flex items-center justify-center">
       {address ? (
-        <div className="flex items-center gap-x-4">
-          <span>{address}</span>
+        <div className="flex items-center gap-4">
+          <div>{address}</div>
           <button
             onClick={async () => {
               await client?.disconnect();
               setAddress("");
+              localStorage.removeItem(addressStorageKey);
             }}
           >
             Logout
@@ -54,16 +59,16 @@ function App() {
         <button
           onClick={async () => {
             try {
-              if(initRef.current) {
-                alert("正在初始化，请稍后再试")
+              if (initRef.current) {
+                alert("正在初始化，请稍后再试");
                 return;
               }
               if (!client) {
-                alert('初始化失败！请刷新页面重试');
+                alert("初始化失败！请刷新页面重试");
                 return;
               }
               if (client.connected()) {
-                // 关闭之前的 connected session
+                // 关闭之前的 connected session, 重新唤起登录弹窗选择钱包账户
                 await client.disconnect();
               }
               // 初始化一个新的 session
@@ -80,7 +85,16 @@ function App() {
               });
               if (session) {
                 console.log("poc: ", session, client);
-                setAddress(session.namespaces["eip155"].accounts[0]);
+                const addressWithChain = session.namespaces["eip155"].accounts[0];
+                // 签名
+                const hexMsg = toHex("Hello world3");
+                const signature = await client.request<string>({
+                  method: "personal_sign",
+                  params: [hexMsg, addressWithChain.split(":")[2]],
+                });
+                console.log("poc", signature);
+                setAddress(addressWithChain);
+                localStorage.setItem(addressStorageKey, addressWithChain);
               } else {
                 throw new Error("Init session failed!");
               }
@@ -90,7 +104,7 @@ function App() {
             }
           }}
         >
-          Login
+          Login And Sign
         </button>
       )}
     </div>
@@ -98,3 +112,12 @@ function App() {
 }
 
 export default App;
+
+function toHex(str: string) {
+  return `0x${str
+    .split("")
+    .map((char: string) => {
+      return ("0" + char.charCodeAt(0).toString(16)).slice(-2);
+    })
+    .join("")}`;
+}
